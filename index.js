5663560473bab39e88c27c6fdf44613dd05f5f6a
orsonbraines <obraines@gmail.com>
2016-09-17 11:24:46 -0400

    draw unicorn on canvas

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 (function(){
   var canvas = document.getElementById('canvas');
+  var unicornImage = document.getElementById('unicorn');
   var g = canvas.getContext('2d');
   g.fillStyle = '#ff0000';
-  g.fillRect(100,100,100,100);
+  g.fillRect(300,300,100,100);
+  g.drawImage(unicornImage,0,0,261,280);
 })();
