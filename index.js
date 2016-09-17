4d7d18e9ff6819ea9915c1c8284512dd2410f678
orsonbraines <obraines@gmail.com>
2016-09-17 14:08:18 -0400

    -added outlines to pieces -refactoring

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 (function(){
-  //w1 and h1 are the height of the src
+  var offset = function(x, y, w, h){
+    return (y*w + x) * 4;
+  }
+  var write = function(imageData, x, y, r, g, b, a){
+    var dataOffset = offset(x,y,imageData.width);
+    imageData.data[dataOffset] = r;
+    imageData.data[dataOffset + 1] = g;
+    imageData.data[dataOffset + 2] = b;
+    imageData.data[dataOffset + 3] = a;
+  }
+  //w1 is the width of the src
   //w and h are the width of the dest
-  var copyPixels = function(src,dest,x,y,w,h,w1,h1){
+  var copyPixels = function(src,dest,x,y,w,h,w1){
     for(var px = 0; px<w ; px ++){
       for(var py = 0; py<h ; py ++){
-        dest.data[(py * w + px) * 4] = src.data[((y + py)*w1 + (x+px))*4]; 
-        dest.data[(py * w + px) * 4 + 1] = src.data[((y + py)*w1 + (x+px))*4 + 1]; 
-        dest.data[(py * w + px) * 4 + 2] = src.data[((y + py)*w1 + (x+px))*4 + 2]; 
-        dest.data[(py * w + px) * 4 + 3] = src.data[((y + py)*w1 + (x+px))*4 + 3]; 
+        var destOffset = offset(px, py, w);
+        var srcOffset = offset(x + px, y + py, w1);
+        dest.data[destOffset] = src.data[srcOffset]; 
+        dest.data[destOffset + 1] = src.data[srcOffset + 1]; 
+        dest.data[destOffset + 2] = src.data[srcOffset + 2]; 
+        dest.data[destOffset + 3] = src.data[srcOffset + 3]; 
       }
     }
+  }
+  
+  //add an outline
+  var outline = function(imageData, r, g, b, lw){
+    var w = imageData.width;
+    var h = imageData.height;
+  //top
+    for(var x=0; x<w; x++)
+      for(var y=0; y<lw; y++)
+        write(imageData,x, y, r, g, b, 255);
+    //bottom
+    for(var x=0; x<w; x++)
+      for(var y=h-lw; y<h; y++)
+        write(imageData,x, y, r, g, b, 255);
+    //left
+    for(var x=0; x<lw; x++)
+      for(var y=0; y<h; y++)
+        write(imageData,x, y, r, g, b, 255);
+    //right
+    for(var x=w-lw; x<w; x++)
+      for(var y=0; y<h; y++)
+        write(imageData,x, y, r, g, b, 255);
+      
   }
   
   var canvas = document.getElementById('canvas');
@@ -27,7 +62,8 @@
     for(var y=0;y<gridSize;y++){
       grid[x][y] = g.getImageData(0, 0, imageWidth/gridSize, imageHeight/gridSize);
       copyPixels(imageData,grid[x][y],x * grid[x][y].width, y * grid[x][y].height, grid[x][y].width, grid[x][y].height,
-        imageWidth, imageHeight);
+        imageWidth);
+      outline(grid[x][y],0,0,0,2);
     }
   }
   
@@ -50,8 +86,8 @@
     g.fillStyle = '#fff';
     g.fillRect(0,0,canvas.width,canvas.height);
     
-    for(var x=0;x<gridSize;x++){
-      for(var y=0;y<gridSize;y++){
+    for(var x = gridSize - 1;x>=0;x--){
+      for(var y = gridSize - 1;y>=0;y--){
         g.putImageData(pieces[x][y].imageData, pieces[x][y].pos.x, pieces[x][y].pos.y);
       }
     }
@@ -68,7 +104,10 @@
       for(var y=0;y<gridSize;y++){
         var r = pieces[x][y].pos;
         var s = pieces[x][y].size;
-        if(startX >= r.x && startX <= r.x + s.x && startY >= r.y && startY <= r.y + s.y) activeElement = pieces[x][y];
+        if(startX >= r.x && startX <= r.x + s.x && startY >= r.y && startY <= r.y + s.y){
+          activeElement = pieces[x][y];
+          return;
+        }
       }
     }
   });
